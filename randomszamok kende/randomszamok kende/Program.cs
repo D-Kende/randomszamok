a3be4795eff5bb52d3e73ba6bc8d46e75c564617
Khende <112852648+Khende@users.noreply.github.com>
2022-09-13 12:02:41 +0200

    Add files via upload

---
+++ randomszamok kende/randomszamok kende/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace randomszamok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] tomb = new int[40];
+            int poz = 0;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                tomb[i] = rnd.Next(-100, 101);
+                Console.Write(tomb[i]);
+                Console.Write(" ");
+                if (tomb[i] > 0)
+                {
+                    poz++;
+                }
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pozitív számok: " + poz + " db");
+
+
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
